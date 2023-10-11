ad27f2cd29069935d922a66918d89b00294cad24
funneljai212 <93401587+funneljai212@users.noreply.github.com>
2023-10-11 10:02:05 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+function gold() {
+document.body.style.backgroundImage = "url('assets/img/bgGold.jpg')";
+}
+function blue() {
+document.body.style.backgroundImage = "url('assets/img/bgBlue.jpg')";
+}
+function red() {
+document.body.style.backgroundImage = "url('assets/img/bgRed.jpg')";
+}
+function white() {
+document.body.style.backgroundImage = "url('assets/img/bgWhite.jpg')";
+}
+function black() {
+document.body.style.backgroundImage = "url('assets/img/bgBlack.jpg')";
+}
