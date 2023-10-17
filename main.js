4eeb2e4d3d09b601a7ab51854a670e91b7207657
funneljai212 <93401587+funneljai212@users.noreply.github.com>
2023-10-17 13:26:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,12 @@ document.body.style.backgroundImage = "url('assets/img/bgWhite.jpg')";
 function black() {
 document.body.style.backgroundImage = "url('assets/img/bgBlack.jpg')";
 }
+function h1() {
+var x = y+1
+  if (x=2) {
+    document.h1.style.color ="Tomato";
+  }
+  if (x=3) {
+    document.h1.style.color ="Green";
+  }
+}
