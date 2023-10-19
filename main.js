0523ab3b77374902b2b293a4f1d62f6a5f772e01
funneljai212 <93401587+funneljai212@users.noreply.github.com>
2023-10-19 11:08:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 function gold() {
 document.body.style.backgroundImage = "url('assets/img/bgGold.jpg')";
+setCookie("gold", user, 30);
 }
 function blue() {
 document.body.style.backgroundImage = "url('assets/img/bgBlue.jpg')";
+setCookie("background", 1 ,30);
 }
 function red() {
 document.body.style.backgroundImage = "url('assets/img/bgRed.jpg')";
+setCookie("red", user, 30);
 }
 function white() {
 document.body.style.backgroundImage = "url('assets/img/bgWhite.jpg')";
+setCookie("white", user, 30);
 }
 function black() {
 document.body.style.backgroundImage = "url('assets/img/bgBlack.jpg')";
+setCookie("black", user, 30);
 }
-function bye() {
-alert("bye bye") && window.close();
+function first() {
+  document.body.write("hello this is my javascript");
+}
+
+document.cookie = "background=x;";
+function body() {
+if (background=1) {
+document.body.style.backgroundImage = "url('assets/img/bgBlue.jpg')";
+} else {
+document.body.style.backgroundImage = "url('assets/img/bgBlack.jpg')";
+}
 }
