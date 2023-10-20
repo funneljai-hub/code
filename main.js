fb3a9ef2a15f30a5d8619572d57bc1f84f860a0e
funneljai212 <93401587+funneljai212@users.noreply.github.com>
2023-10-20 11:50:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 function gold() {
 document.body.style.backgroundImage = "url('assets/img/bgGold.jpg')";
-document.cookie = "background=gold";
+document.cookie = "background=1";
 }
 function blue() {
 document.body.style.backgroundImage = "url('assets/img/bgBlue.jpg')";
-document.cookie = "background=blue";
+document.cookie = "background=2";
 }
 function red() {
 document.body.style.backgroundImage = "url('assets/img/bgRed.jpg')";
-document.cookie = "background=red";
+document.cookie = "background=3";
 }
 function white() {
 document.body.style.backgroundImage = "url('assets/img/bgWhite.jpg')";
-document.cookie = "background=white";
+document.cookie = "background=4";
 }
 function black() {
 document.body.style.backgroundImage = "url('assets/img/bgBlack.jpg')";
-document.cookie = "background=black";
+document.cookie = "background=0";
 }
 function first() {
   document.body.write("hello this is my javascript");
 }
 
 function b() {
-if (background=black) {
+if (background=0) {
 document.body.style.backgroundImage = "url('assets/img/bgBlack.jpg')";
 }
-if (background=blue) {
+if (background=2) {
 document.body.style.backgroundImage = "url('assets/img/bgBlue.jpg')";
 }
-if (background=gold) {
+if (background=1) {
 document.body.style.backgroundImage = "url('assets/img/bgGold.jpg')";
 }
-if (background=red) {
+if (background=3) {
 document.body.style.backgroundImage = "url('assets/img/bgRed.jpg')";
 } 
 alert(background);
