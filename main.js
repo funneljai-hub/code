ee56938b24de9583d208d95d122fc6abd799716c
funneljai212 <93401587+funneljai212@users.noreply.github.com>
2023-10-20 12:44:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function first() {
   document.body.write("hello this is my javascript");
 }
 document.cookie = "background=x";
-function b() {
+function back() {
 if ('background'=0) {
 document.body.style.backgroundImage = "url('assets/img/bgBlack.jpg')";
 }
