14d1d7813c02f4968a52bef0dbcaa420d85c0d79
funneljai212 <93401587+funneljai212@users.noreply.github.com>
2023-10-25 12:05:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,41 @@
-function setCookie(cName, cValue, expDays) {
-        let date = new Date();
-        date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
-        const expires = "expires=" + date.toUTCString();
-        document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
-}
+
 function gold() {
 document.body.style.backgroundImage = "url('assets/img/bgGold.jpg')";
-setCookie('background', 1, 30);
+document.cookie ="background=1;path=/main/";
 }
 function blue() {
 document.body.style.backgroundImage = "url('assets/img/bgBlue.jpg')";
-setCookie('background', 2, 30);
+document.cookie ="background=2;path=/main/";
 }
 function red() {
 document.body.style.backgroundImage = "url('assets/img/bgRed.jpg')";
-setCookie('background', 3, 30);
+document.cookie ="background=3;path=/main/";
 }
 function white() {
 document.body.style.backgroundImage = "url('assets/img/bgWhite.jpg')";
-setCookie('background', 4, 30);
+document.cookie ="background=4;path=/main/";
 }
 function black() {
 document.body.style.backgroundImage = "url('assets/img/bgBlack.jpg')";
-setCookie('background', 0, 30);
+document.cookie ="background=0;path=/main/";
 }
 function first() {
   document.body.write("hello this is my javascript");
 }
 function b() {
-if (background = 0) {
+if (background == 0) {
 document.body.style.backgroundImage = "url('assets/img/bgBlack.jpg')";
 }
-if (background =2) {
+if (background ==2) {
 document.body.style.backgroundImage = "url('assets/img/bgBlue.jpg')";
 }
-if (background =1) {
+if (background ==1) {
 document.body.style.backgroundImage = "url('assets/img/bgGold.jpg')";
 }
-if (background =3) {
+if (background ==3) {
 document.body.style.backgroundImage = "url('assets/img/bgRed.jpg')";
 } 
-if (background =4) {
+if (background == 4) {
 document.body.style.backgroundImage = "url('assets/img/bgWhite.jpg')";
 } 
 alert(background);
